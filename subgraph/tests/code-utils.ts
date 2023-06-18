5b337be890ec7f3857ddd6e84c620d05c66df3cb
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2023-06-18 15:35:32 -0400

    Code token initialized

---
+++ subgraph/tests/code-utils.ts
@@ -0,0 +1,227 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  DelegateChanged,
+  DelegateVotesChanged,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Sweep20,
+  Sweep721,
+  Transfer
+} from "../generated/CODE/CODE"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createDelegateChangedEvent(
+  delegator: Address,
+  fromDelegate: Address,
+  toDelegate: Address
+): DelegateChanged {
+  let delegateChangedEvent = changetype<DelegateChanged>(newMockEvent())
+
+  delegateChangedEvent.parameters = new Array()
+
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam("delegator", ethereum.Value.fromAddress(delegator))
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "fromDelegate",
+      ethereum.Value.fromAddress(fromDelegate)
+    )
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "toDelegate",
+      ethereum.Value.fromAddress(toDelegate)
+    )
+  )
+
+  return delegateChangedEvent
+}
+
+export function createDelegateVotesChangedEvent(
+  delegate: Address,
+  previousBalance: BigInt,
+  newBalance: BigInt
+): DelegateVotesChanged {
+  let delegateVotesChangedEvent = changetype<DelegateVotesChanged>(
+    newMockEvent()
+  )
+
+  delegateVotesChangedEvent.parameters = new Array()
+
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam("delegate", ethereum.Value.fromAddress(delegate))
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousBalance",
+      ethereum.Value.fromUnsignedBigInt(previousBalance)
+    )
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newBalance",
+      ethereum.Value.fromUnsignedBigInt(newBalance)
+    )
+  )
+
+  return delegateVotesChangedEvent
+}
+
+export function createRoleAdminChangedEvent(
+  role: Bytes,
+  previousAdminRole: Bytes,
+  newAdminRole: Bytes
+): RoleAdminChanged {
+  let roleAdminChangedEvent = changetype<RoleAdminChanged>(newMockEvent())
+
+  roleAdminChangedEvent.parameters = new Array()
+
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdminRole",
+      ethereum.Value.fromFixedBytes(previousAdminRole)
+    )
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newAdminRole",
+      ethereum.Value.fromFixedBytes(newAdminRole)
+    )
+  )
+
+  return roleAdminChangedEvent
+}
+
+export function createRoleGrantedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleGranted {
+  let roleGrantedEvent = changetype<RoleGranted>(newMockEvent())
+
+  roleGrantedEvent.parameters = new Array()
+
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleGrantedEvent
+}
+
+export function createRoleRevokedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleRevoked {
+  let roleRevokedEvent = changetype<RoleRevoked>(newMockEvent())
+
+  roleRevokedEvent.parameters = new Array()
+
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleRevokedEvent
+}
+
+export function createSweep20Event(_token: Address, _to: Address): Sweep20 {
+  let sweep20Event = changetype<Sweep20>(newMockEvent())
+
+  sweep20Event.parameters = new Array()
+
+  sweep20Event.parameters.push(
+    new ethereum.EventParam("_token", ethereum.Value.fromAddress(_token))
+  )
+  sweep20Event.parameters.push(
+    new ethereum.EventParam("_to", ethereum.Value.fromAddress(_to))
+  )
+
+  return sweep20Event
+}
+
+export function createSweep721Event(
+  _token: Address,
+  _to: Address,
+  _tokenID: BigInt
+): Sweep721 {
+  let sweep721Event = changetype<Sweep721>(newMockEvent())
+
+  sweep721Event.parameters = new Array()
+
+  sweep721Event.parameters.push(
+    new ethereum.EventParam("_token", ethereum.Value.fromAddress(_token))
+  )
+  sweep721Event.parameters.push(
+    new ethereum.EventParam("_to", ethereum.Value.fromAddress(_to))
+  )
+  sweep721Event.parameters.push(
+    new ethereum.EventParam(
+      "_tokenID",
+      ethereum.Value.fromUnsignedBigInt(_tokenID)
+    )
+  )
+
+  return sweep721Event
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
